58f7c1cd9729a8b4c3f6f39b4010517aafe1c0fe
MarcelErpi <marcel.erpenbeck@web.de>
2016-12-05 18:49:10 +0100

    fixed typo

--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -16,7 +16,7 @@ public class CoordinateTest {
     private final double epsilon = 0.0001;
 
     @Test
-    public void testWitchDifferentCoordinates(){
+    public void testWithDifferentCoordinates(){
         SphericCoordinate cd1 = new SphericCoordinate(90, 90);
         CartesianCoordinate cd2 = new CartesianCoordinate(50, 40, 100);
 
